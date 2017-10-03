76c21aa7d3790f22e31c4e6a75733c69c4b508eb
Sayfullah Eid <sayfullaheid@gmail.com>
2017-10-03 14:43:28 -0400

    Cuboid class created Cuboid functions defined: -getVolume -getSurfaceArea -volumeExplain

---
+++ src/com/company/Cuboid.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Cuboid {
+    private float l;
+    private float w;
+    private float h;
+    public Cuboid(float length, float width, float height) {
+        l = length;
+        w = width;
+        h = height;
+    }
+
+    public float getVolume(){
+        return l * h * w;
+    }
+
+    public float getSurfaceArea(){
+        return (2*(w * h)) + (2*(l * h)) + (2*(l * w));
+    }
+
+    public void volumeExplain(){
+        System.out.println("Area of the Cross Section: " + String.format("%.2f", h) + " x " + String.format("%.2f", w) + " = " + String.format("%.2f", (h * w)) );
+        System.out.println("Multiply by the length: " + String.format("%.2f",l) + " x " + String.format("%.2f", (h * w)) +  " = " + getVolume() );
+    }
+    public void surfaceAreaExplain(){
+
+    }
+}
